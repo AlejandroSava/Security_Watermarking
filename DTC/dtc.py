b045816d7aa8c775cda96a7b0b50143718b2c806
AlejandroSava <alexandu.choric@gmail.com>
2025-06-21 11:07:09 -0600

    adding RGB channels

--- DTC/dtc.py
+++ DTC/dtc.py
@@ -63,11 +63,13 @@ def psnr_measurement(original_path, modified_path, grayscale=True):
 # --- --- --- --- --- --- #
 
 class StegoDTC:
-    def __init__(self, img_path="image.png", message="WaterMarking"):
+    def __init__(self, img_path="image.png", message="WaterMarking", message2=None, message3=None):
         self.image_path = img_path
         self.message = message
+        self.message2 = message2 # for RGB
+        self.message3 = message3 # for RBG
         # Coordinates of DCT coefficients to be modified
-        self.u1, self.v1 = 2, 3
+        self.u1, self.v1 = 2, 1
         self.u2, self.v2 = 3, 3
         self.x = 10.0  # Minimum difference between coefficients for robustness
         self.block_size = 8  # Image is processed in 8x8 blocks
@@ -95,7 +97,7 @@ class StegoDTC:
 
         return image_opened_gray
 
-    def load_image(self):
+    def load_gray_image(self):
         """
         Loads the grayscale image, stores dimensions, and crops it to fit 8x8 blocks.
         """
@@ -106,6 +108,29 @@ class StegoDTC:
         print("The maximum size of bytes is:", self.max_mark_size)
         print("The message size is:", len(self.message))
         self.img = self.img[:self.h - self.h % self.block_size, :self.w - self.w % self.block_size]
+
+
+    def load_image_rgb(self, image_path=None):
+        """
+        Loads the image in color (BGR), stores its dimensions, and crops it to fit 8x8 blocks.
+        """
+        if image_path is None:
+            image_path = self.image_path
+
+        self.img = cv2.imread(image_path)
+        cv2.imshow("Original_" + image_path, self.img)
+        if self.img is None:
+            raise ValueError("Image could not be loaded.")
+
+        self.h, self.w, _ = self.img.shape
+        print("Original shape:", self.img.shape)
+        self.max_mark_size = self.h * self.w // 64 // 8
+        print("Maximum embeddable bytes:", self.max_mark_size)
+        print("Message size:", len(self.message))
+
+        # Crop image to be divisible by block_size
+        self.img = self.img[:self.h - self.h % self.block_size, :self.w - self.w % self.block_size]
+        self.h, self.w, _ = self.img.shape
 
     def embedded_bit(self, block, bit):
         """
@@ -139,16 +164,19 @@ class StegoDTC:
         # Convert block back to spatial domain
         return np.round(idct2(B)).clip(0, 255).astype(np.uint8)
 
-    def create_marking_text2bin_vector(self):
+    def create_marking_text2bin_vector(self, message=None):
         """
         Converts the input message to a binary vector (ASCII per character).
         """
+        if message is None:
+            message = self.message
+        
         bin_vector = []
-        for character in self.message:
+        for character in message:
             for i in range(7, -1, -1):
                 bit_character = (ord(character) >> i) & 1
                 bin_vector.append(bit_character)
-        print("The bit vector is: ", bin_vector)
+        #print("The bit vector is: ", bin_vector)
         return bin_vector
 
     def embedded_text_message(self):
@@ -202,15 +230,93 @@ class StegoDTC:
                 extracted_bits.append(bit)
                 index += 1
 
-        print("The extracted message is:", extracted_bits)
+        #print("The extracted message is:", extracted_bits)
 
         # Group bits into 8-bit chunks and convert to characters
         chunks = [extracted_bits[i:i + 8] for i in range(0, len(extracted_bits), 8)]
         marking_recovered = ''.join([chr(int(''.join(map(str, byte)), 2)) for byte in chunks])
         print(f"Recovered watermarking:")
         print(marking_recovered)
+        return marking_recovered
 
-    def finish_opencv_session(self):
+    def embedded_text_message_rgb(self):
+        """
+        Embed the binary message into the blue channel of the RGB image.
+        """
+        # Split image into BGR channels
+        b_channel, g_channel, r_channel = cv2.split(self.img)
+        # Pair each channel with its respective message
+        channels = [b_channel, g_channel, r_channel]
+        messages = [self.message, self.message2, self.message3]
+
+        stego_channels = []
+        for channel, message in zip(channels, messages):
+            text2bin_vector = self.create_marking_text2bin_vector(message)
+            embedded_blocks = []
+            index = 0
+            for i in range(0, self.h, self.block_size):
+                for j in range(0, self.w, self.block_size):
+                    block = channel[i:i + self.block_size, j:j + self.block_size]
+                    if index < len(text2bin_vector):
+                        embedded = self.embedded_bit(block, text2bin_vector[index])
+                        embedded_blocks.append(((i, j), embedded))
+                        index += 1
+                    else:
+                        embedded_blocks.append(((i, j), block))
+
+            # # Reconstruct the modified channel
+            stego_channel = np.zeros_like(channel)
+            for (i, j), block in embedded_blocks:
+                stego_channel[i:i + self.block_size, j:j + self.block_size] = block
+            stego_channels.append(stego_channel)
+        # Merge channels and save
+        self.stego_img = cv2.merge(stego_channels)
+        cv2.imshow("stego_rgb_dtc_" + self.image_path, self.stego_img)
+        cv2.imwrite("stego_rgb_dtc_" + self.image_path, self.stego_img)
+
+    def recover_text_watermarking_rgb(self, message_1_len, message_2_len, message_3_len, path_name: str):
+        """
+        Recover the watermarks from the RGB channels of the stego image.
+        """
+        stego_img = cv2.imread(path_name)
+        if stego_img is None:
+            raise ValueError("Stego image could not be loaded.")
+
+        h, w, _ = stego_img.shape
+        b_channel, g_channel, r_channel = cv2.split(stego_img)  # <-- FIX: use stego_img not self.img
+        channels = [b_channel, g_channel, r_channel]
+        messages_len = [message_1_len, message_2_len, message_3_len]
+        channel_names = ["Blue", "Green", "Red"]
+
+        extracted_messages = []
+
+        for channel, message_len, name in zip(channels, messages_len, channel_names):
+            extracted_bits = []
+            index = 0
+            for i in range(0, h, self.block_size):
+                if index >= (message_len * 8):
+                    break
+                for j in range(0, w, self.block_size):
+                    if index >= (message_len * 8):
+                        break
+                    block = channel[i:i + self.block_size, j:j + self.block_size]
+                    bit = self.extract_bit(block)
+                    extracted_bits.append(bit)
+                    index += 1
+
+            # Convert bits to characters
+
+            chunks = [extracted_bits[i:i + 8] for i in range(0, len(extracted_bits), 8)]
+            recovered_message = ''.join([chr(int(''.join(map(str, byte)), 2)) for byte in chunks])
+            extracted_messages.append(recovered_message)
+            # Truncar a los bits esperados
+
+
+            print(f"Recovered watermark from {name} channel: {recovered_message}")
+        return extracted_messages
+
+    @staticmethod
+    def finish_opencv_session():
         """
         Finalizes OpenCV windows.
         """
@@ -219,12 +325,33 @@ class StegoDTC:
         print("Finish all the OpenCV sessions")
 
 
-# --- Execution ---
-watermarking = "Alejandro Salinas Victorino, Esta es mi Marca de Agua"
-image_name = "image.png"
-watermarking_dtc = StegoDTC(img_path=image_name, message=watermarking)     # Initialize with image file
-watermarking_dtc.load_image()                # Load and preprocess image
-watermarking_dtc.embedded_text_message()     # Embed watermark into image
-watermarking_dtc.recover_text_watermarking(message_len=len(watermarking), path_name="stego_dtc_"+image_name) # Recover watermark
-psnr_measurement(original_path=image_name, modified_path="stego_dtc_"+image_name)
-watermarking_dtc.finish_opencv_session()     # Close windows
+
+def main_gray_scale():
+    watermarking = "Alejandro Salinas Victorino, Esta es mi Marca de Agua. Saludos!"
+    image_name = "image.png"
+    watermarking_dtc = StegoDTC(img_path=image_name, message=watermarking)  # Initialize with image file
+    watermarking_dtc.load_gray_image()  # Load and preprocess image
+    watermarking_dtc.embedded_text_message()  # Embed watermark into image
+    watermarking_dtc.recover_text_watermarking(message_len=len(watermarking),
+                                               path_name="stego_dtc_" + image_name)  # Recover watermark
+    psnr_measurement(original_path=image_name, modified_path= "stego_dtc_" + image_name)
+    watermarking_dtc.finish_opencv_session()
+
+def main_rgb_scale():
+    watermarking_rgb_1 = "Alejandro Salinas Victorino, Esta es mi Marca de Agua, saludos"
+    watermarking_rgb_2 = "Esta es mi Segunda Marca de agua que he incrustado"
+    watermarking_rgb_3 = "Incrustando una Tercera Marca de agua para que sea recuperada"
+    image_name_rgb = "image.png"
+    watermarking_rgb_dtc = StegoDTC(img_path=image_name_rgb, message=watermarking_rgb_1, message2=watermarking_rgb_2, message3=watermarking_rgb_3)
+    watermarking_rgb_dtc.load_image_rgb()
+    watermarking_rgb_dtc.embedded_text_message_rgb()
+    watermarking_rgb_dtc.recover_text_watermarking_rgb(message_1_len=len(watermarking_rgb_1),
+                                                       message_2_len=len(watermarking_rgb_2),
+                                                       message_3_len=len(watermarking_rgb_3),
+                                                       path_name="stego_rgb_dtc_" + image_name_rgb)
+    psnr_measurement(original_path=image_name_rgb, modified_path="stego_rgb_dtc_" + image_name_rgb)
+    watermarking_rgb_dtc.finish_opencv_session()
+
+if __name__ == "__main__":
+    main_gray_scale()
+    main_rgb_scale()
